c241dcf50853281ce24eb24fade71203f9b893ba
kobirul5 <kobirul05j@gmail.com>
2024-11-24 12:48:34 +0600

    added node js gile

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const users= [
+    {id: 1, name: 'Sabana', email: 'sabana@gamil.com'},
+    {id: 2, name: 'sokina', email: 'sokina@gamil.com'},
+    {id: 3, name: 'Sabnoor', email: 'zabnoor@gamil.com'},
+]
+
+app.get('/', (req, res)=>{
+    res.send("User management server is running")
+});
+app.get('/users', (req, res)=>{
+    res.send(users)
+});
+app.post('/users', (req, res)=>{
+    console.log("API hitting")
+    console.log(req.body);
+
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on PORT: ${port}`)
+})
